b4335b364dc81adf6b5b429a961bb1ed1e2e3b07
Takeharu Oshida <takeharu.oshida@mobilus.co.jp>
2013-10-06 16:56:05 +0900

    Convert mysql To H2

--- src/main/scala/isucon3/Mysql2H2.scala
+++ src/main/scala/isucon3/Mysql2H2.scala
@@ -2,6 +2,7 @@ package isucon3
 
 import java.sql.{Connection, DriverManager, Statement}
 import isucon3.model.DB
+import java.text.SimpleDateFormat
 
 object Mysql2H2 {
   def main(args: Array[String]) {
@@ -13,7 +14,7 @@ object Mysql2H2 {
 
   def convert() {
     Class.forName("com.mysql.jdbc.Driver")
-    val my = DriverManager.getConnection("jdbc:mysql://localhost:3306/isucon3?user=root&password=root&useUnicode=true&characterEncoding=UTF-8")
+    val my = DriverManager.getConnection("jdbc:mysql://localhost:3306/isucon?user=root&password=root&useUnicode=true&characterEncoding=UTF-8")
     val h2 = DB.cp.getConnection()
 
     resetH2(h2)
@@ -31,22 +32,59 @@ object Mysql2H2 {
 
   def convertMemos(my: Connection, h2: Connection) {
     var s = my.createStatement()
-    val r = s.executeQuery("SELECT * FROM memos ORDER BY id LIMIT 100 OFFSET 0")
+    var s2 = h2.createStatement()
+    val r = s.executeQuery("SELECT memos.* ,users.username FROM memos, users WHERE memos.user = users.id ORDER BY id")
+
     while (r.next()) {
-      val id        = r.getInt("id")
-      val content   = r.getString("content")
-      val isPrivate = r.getInt("is_private")
+      val id            = r.getInt("id")
+      val user          = r.getInt("user")
+      val username      = r.getString("username")
+      val content       = r.getString("content")
+      val isPrivate     = r.getInt("is_private")
+      val created_at    = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss").format(r.getDate("created_at")).toString()
+      val updated_at    = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss").format(r.getDate("updated_at")).toString()
 
+      //memo["content"].split(/\r?\n/).first
+      val contentLine = content.lines
+      val title = contentLine.next()
 
-      val title = content  // FIXME
-
-
-      println(id)
+      var ps = h2.prepareStatement("INSERT INTO memos(id, title, content, is_private, created_at, updated_at, user, username)VALUES(?,?,?,?,?,?,?,?)")
+      ps.setInt(1, id)
+      ps.setString(2, title)
+      ps.setString(3, content)
+      ps.setInt(4, isPrivate)
+      ps.setString(5, created_at)
+      ps.setString(6, updated_at)
+      ps.setInt(7, user)
+      ps.setString(8, username)
+      ps.execute()
+      ps.close()
     }
     s.close()
+    s2.close()
   }
 
   def convertUsers(h2: Connection, my: Connection) {
+    var s = my.createStatement()
+    var s2 = h2.createStatement()
+    val r = s.executeQuery("SELECT * FROM users ORDER BY id")
+    while (r.next()) {
+      val id          = r.getInt("id")
+      val username    = r.getString("user")
+      val password    = r.getString("password")
+      val salt        = r.getString("salt")
+      val last_access = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss").format(r.getDate("last_access")).toString()
 
+      var ps = h2.prepareStatement("INSERT INTO users(id, username, password, password, password)VALUES(?,?,?,?,?)")
+      ps.setInt(1, id)
+      ps.setString(2, username)
+      ps.setString(3, password)
+      ps.setString(4, salt)
+      ps.setString(5, last_access)
+      ps.execute()
+      ps.close()
+    }
+    s.close()
+    s2.close()
   }
 }
