76252d1b0606746e6bd173513192cb1243f7dcbb
Ngoc Dao <ngocdaothanh@gmail.com>
2013-10-06 16:44:34 +0900

    Fix Recent

--- src/main/scala/isucon3/action/Recent.scala
+++ src/main/scala/isucon3/action/Recent.scala
@@ -1,10 +1,23 @@
 package isucon3.action
 
+import org.jboss.netty.handler.codec.http.HttpResponseStatus
 import xitrum.annotation.GET
+
+import isucon3.model.DB
+import isucon3.view.IndexView
 
 @GET("recent/:page")
 class Recent extends DefaultLayout {
   def execute() {
-    respondView()
+    val page  = param[Int]("page")
+    val total = DB.countPublicMemos()
+    val memos = DB.getRecentPublicMemos(page)
+
+    if (memos.isEmpty) {
+      response.setStatus(HttpResponseStatus.NOT_FOUND)
+      respondText("404 Not Found")
+    } else {
+      respondInlineView(IndexView.render(this, page, total, memos))
+    }
   }
 }
