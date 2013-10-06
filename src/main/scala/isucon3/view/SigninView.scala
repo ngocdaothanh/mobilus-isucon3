3c798a969673569341e3fca36961bdb90c655410
Ngoc Dao <ngocdaothanh@gmail.com>
2013-10-06 11:46:09 +0900

    Add SigninView

---
+++ src/main/scala/isucon3/view/SigninView.scala
@@ -0,0 +1,16 @@
+package isucon3.view
+
+import scalatags._
+import xitrum.Action
+import isucon3.action.DoSignin
+
+object SigninView {
+  def render(action: Action) = Seq(
+form.action(action.url[DoSignin]).attr("method" -> "post")(
+  "username", input.attr("type" -> "text", "name" -> "username", "size" -> "20"),
+  br,
+  "password", input.attr("type" -> "password", "name" -> "password", "size" -> "20"),
+  br,
+  input.attr("type" -> "submit", "value" -> "signin")
+))
+}
