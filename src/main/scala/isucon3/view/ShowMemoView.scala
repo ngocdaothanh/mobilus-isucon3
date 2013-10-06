25ae38edde8c89073c22651aec456b754cdf6f2a
Ngoc Dao <ngocdaothanh@gmail.com>
2013-10-06 12:08:55 +0900

    Add ShowMemoView

---
+++ src/main/scala/isucon3/view/ShowMemoView.scala
@@ -0,0 +1,32 @@
+package isucon3.view
+
+import scalatags._
+import xitrum.Action
+
+import isucon3.action.ShowMemo
+import isucon3.model.Memo
+
+object ShowMemoView {
+  def render(action: Action, memo: Memo, older: Option[Int], newer: Option[Int]) = Seq(
+p.id("author")(
+  if (memo.isPrivate) "Private" else "Public",
+  "Memo by ", memo.username, memo.createdAt
+),
+
+hr,
+if (older.isDefined)
+  a.id("older").href(action.url[ShowMemo]("memoId" -> older.get))("< older memo")
+else
+  ""
+,
+"|",
+if (newer.isDefined)
+  a.id("older").href(action.url[ShowMemo]("memoId" -> newer.get))("newer memo >")
+else
+  ""
+,
+
+hr,
+div.id("content_html")(memo.contentHtml)
+  )
+}
